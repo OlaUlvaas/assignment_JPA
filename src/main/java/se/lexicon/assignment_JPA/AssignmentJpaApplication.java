eb477e7014596d8164d1f300b13242bd19a88905
olaulvas <ola.ulvaas@gmail.com>
2021-03-09 09:37:51 +0100

    setup project with readme, connection to mysql

---
+++ src/main/java/se/lexicon/assignment_JPA/AssignmentJpaApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.assignment_JPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/lexicon/assignment_JPA/AssignmentJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.assignment_JPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
